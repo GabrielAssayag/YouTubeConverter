fc842d66d662dfaa8b02ace492e64310d064c03a
DEV07\gabriel.a <gabriel.a@starkey.co.il>
2022-05-22 16:42:51 +0300

    adding user input. general information output. Download file in ~/PATH

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 # My very own youtube converter based on Python3. Woho!
 
+from pytube import YouTube
+
+#ask for the link from user
+link = input("Enter the link of YouTube video you want to download: ")
+yt = YouTube(url=link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+
+#Getting the highest resolution possible
+ys = yt.streams.get_audio_only()
+
+#Starting download
+print("Downloading...")
+ys.download()
+print("Download completed!!")
